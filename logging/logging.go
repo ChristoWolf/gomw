1a437e2469789e6bb4fb32002dacd62aeb96ce13
ChristoWolf <christoph.n.wolf@gmail.com>
2022-08-26 17:03:35 +0200

    Implemented and tested logging middleware.
    Related: #1

---
+++ logging/logging.go
@@ -0,0 +1,70 @@
+// Package logging implements a simple logger middleware.
+package logging
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+// wrappedResponseWriter is a wrapper around
+// http.ResponseWriter that caches additional
+// logging relevant information.
+type wrappedResponseWriter struct {
+	http.ResponseWriter
+	body   []byte
+	status int
+}
+
+// NewWrappedResponseWriter creates a new
+// wrappedResponseWriter with overwritable
+// default values which will be filled with
+// request execution information.
+func NewWrappedResponseWriter(w http.ResponseWriter) *wrappedResponseWriter {
+	return &wrappedResponseWriter{
+		ResponseWriter: w,
+		body:           []byte{},
+		status:         http.StatusInternalServerError}
+}
+
+// WriteHeader partially implements http.ResponseWriter.
+// It caches the status code and calls the underlying
+// http.ResponseWriter.WriteHeader.
+func (w *wrappedResponseWriter) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
+// Write partially implements http.ResponseWriter.
+// It caches the response body and calls the underlying
+// http.ResponseWriter.Write.
+func (w *wrappedResponseWriter) Write(b []byte) (int, error) {
+	w.body = b
+	return w.ResponseWriter.Write(b)
+}
+
+// Middleware is a middleware for logging information
+// about the request and response.
+// TODO: Log more information (sizes, execution time, etc).
+func Middleware(logger log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
+			wrapped := NewWrappedResponseWriter(w)
+			body, err := io.ReadAll(r.Body)
+			logger.Printf("Request: %s %s %s\n", r.Method, r.URL, body)
+			if err != nil {
+				err := fmt.Errorf("error reading request body: %w", err)
+				logger.Printf("%v\n", err)
+				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusInternalServerError)
+					fmt.Fprint(w, err)
+				}).ServeHTTP(wrapped, r)
+			} else {
+				next.ServeHTTP(wrapped, r)
+			}
+			logger.Printf("Response: %d %s\n", wrapped.status, string(wrapped.body))
+		})
+	}
+}
---
+++ logging/logging_test.go
@@ -0,0 +1,99 @@
+// Package logging_test implements tests for the logging package.
+package logging_test
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/christowolf/go-middleware/logging"
+)
+
+// TestMiddleware tests the logging middleware.
+func TestMiddleware(t *testing.T) {
+	data := []struct {
+		method string
+		url    string
+		status int
+		resp   string
+	}{
+		{"GET", "/test/endpoint/get", http.StatusOK, "OK"},
+		{"POST", "/test/endpoint/post", http.StatusCreated, "Created"},
+		{"PUT", "/test/endpoint/put", http.StatusAccepted, "OK"},
+		{"DELETE", "/test/endpoint/delete", http.StatusOK, "OK"},
+		{"OPTIONS", "/test/endpoint/notfound", http.StatusNotFound, "Not found"},
+	}
+	t.Parallel()
+	for _, row := range data {
+		row := row
+		t.Run(row.method+" "+row.url, func(t *testing.T) {
+			t.Parallel()
+			handlerStub := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(row.status)
+				fmt.Fprint(w, row.resp)
+			})
+			req := httptest.NewRequest(row.method, row.url, nil)
+			rec := httptest.NewRecorder()
+			writer := strings.Builder{}
+			logger := log.New(&writer, "", log.LstdFlags)
+			sut := logging.Middleware(*logger)(handlerStub)
+			sut.ServeHTTP(rec, req)
+			if rec.Code != row.status {
+				t.Errorf("expected status code %d, got %d", row.status, rec.Code)
+			}
+			if rec.Body.String() != row.resp {
+				t.Errorf("expected response %s, got %s", row.resp, rec.Body.String())
+			}
+			if !strings.Contains(writer.String(), row.method) {
+				t.Errorf("expected log to contain method %s, got %s", row.method, writer.String())
+			}
+			if !strings.Contains(writer.String(), row.url) {
+				t.Errorf("expected log to contain url %s, got %s", row.url, writer.String())
+			}
+			if !strings.Contains(writer.String(), strconv.Itoa(row.status)) {
+				t.Errorf("expected log to contain status %d, got %s", row.status, writer.String())
+			}
+			if !strings.Contains(writer.String(), row.resp) {
+				t.Errorf("expected log to contain response %s, got %s", row.resp, writer.String())
+			}
+		})
+	}
+}
+
+// TestMiddlewareReadErr tests the
+// logging middleware when the response body
+// cannot be read.
+func TestMiddlewareReadErr(t *testing.T) {
+	t.Parallel()
+	// We inject a handler which acts like everything worked.
+	handlerStub := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	})
+	req := httptest.NewRequest("GET", "/test/endpoint/error", errReader{})
+	rec := httptest.NewRecorder()
+	writer := strings.Builder{}
+	logger := log.New(&writer, "", log.LstdFlags)
+	sut := logging.Middleware(*logger)(handlerStub)
+	sut.ServeHTTP(rec, req)
+	wantCode := http.StatusInternalServerError
+	if rec.Code != wantCode {
+		t.Errorf("expected status code %d, got %d", wantCode, rec.Code)
+	}
+	if !strings.Contains(rec.Body.String(), "error") {
+		t.Errorf("expected response body to contain error, got %s", rec.Body.String())
+	}
+	if !strings.Contains(writer.String(), "error") {
+		t.Errorf("expected log to contain error, got %s", writer.String())
+	}
+}
+
+// errReader is an io.Reader that always returns an error.
+type errReader struct{}
+
+// Read always returns an error.
+func (errReader) Read(p []byte) (n int, err error) {
+	return 0, fmt.Errorf("refused to read")
+}
