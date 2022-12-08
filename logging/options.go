76ec8b33419d9ef4343b6d0a5fead4b5c3bdac20
ChristoWolf <christoph.n.wolf@gmail.com>
2022-12-08 15:23:26 +0100

    chore: encapsulated `Option` types (#3)

--- logging/options.go
+++ logging/options.go
@@ -18,9 +18,13 @@ type Options struct {
 	withDuration       bool
 }
 
+// Option is a function that configures the middleware
+// via the functional options pattern.
+type Option func(*Options)
+
 // NewOptions creates functionally injectable Options.
 // If no logger is provided, it defaults to os.Stdout.
-func NewOptions(options ...func(*Options)) *Options {
+func NewOptions(options ...Option) *Options {
 	opts := &Options{logger: log.New(os.Stdout, "", log.LstdFlags)}
 	for _, option := range options {
 		option(opts)
@@ -29,35 +33,35 @@ func NewOptions(options ...func(*Options)) *Options {
 }
 
 // WithLogger configures the logger to use.
-func WithLogger(logger *log.Logger) func(*Options) {
+func WithLogger(logger *log.Logger) Option {
 	return func(o *Options) {
 		o.logger = logger
 	}
 }
 
 // WithBodies configures whether to log request and response bodies.
-func WithBodies(withBodies bool) func(*Options) {
+func WithBodies(withBodies bool) Option {
 	return func(o *Options) {
 		o.withBodies = withBodies
 	}
 }
 
 // WithStatus configures whether to log the response status.
-func WithStatus(withStatus bool) func(*Options) {
+func WithStatus(withStatus bool) Option {
 	return func(o *Options) {
 		o.withStatus = withStatus
 	}
 }
 
 // WithMethod configures whether to log the request method.
-func WithMethod(withMethod bool) func(*Options) {
+func WithMethod(withMethod bool) Option {
 	return func(o *Options) {
 		o.withMethod = withMethod
 	}
 }
 
 // WithUrl configures whether to log the request URL.
-func WithUrl(withUrl bool) func(*Options) {
+func WithUrl(withUrl bool) Option {
 	return func(o *Options) {
 		o.withUrl = withUrl
 	}
@@ -65,14 +69,14 @@ func WithUrl(withUrl bool) func(*Options) {
 
 // WithContentLengths configures whether to log
 // the request's and response's content lengths.
-func WithContentLengths(withContentLengths bool) func(*Options) {
+func WithContentLengths(withContentLengths bool) Option {
 	return func(o *Options) {
 		o.withContentLengths = withContentLengths
 	}
 }
 
 // WithDuration configures whether to log the request duration.
-func WithDuration(withDuration bool) func(*Options) {
+func WithDuration(withDuration bool) Option {
 	return func(o *Options) {
 		o.withDuration = withDuration
 	}
